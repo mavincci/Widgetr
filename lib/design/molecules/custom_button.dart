5091fc5320474b5a7a7684ce826182a61c6615d3
Michael Abebe <notthismichael@gmail.com>
2024-12-29 21:41:31 +0300

    Feat: Added custom button with test molecule gallery

---
+++ lib/design/molecules/custom_button.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String? text;
+  final VoidCallback onPressed;
+  final Color backgroundColor;
+  final Color textColor;
+  final Icon? icon;
+  final double? iconSize;
+  final EdgeInsetsGeometry padding;
+  final bool isIconLeft;
+  final BorderRadius borderRadius;
+  final double borderWidth;
+  final Color? borderColor;
+
+  const CustomButton({
+    super.key,
+    this.text,
+    required this.onPressed,
+    required this.backgroundColor,
+    required this.textColor,
+    this.icon,
+    this.iconSize,
+    this.padding = const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+    this.borderRadius = const BorderRadius.all(Radius.circular(8)),
+    this.isIconLeft = false,
+    this.borderColor,
+    this.borderWidth = 0.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        padding: padding,
+        shape: RoundedRectangleBorder(
+          borderRadius: borderRadius,
+          side: borderWidth > 0
+              ? BorderSide(
+                  color: borderColor ?? Colors.transparent,
+                  width: borderWidth,
+                )
+              : BorderSide.none,
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (icon != null && !isIconLeft)
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Icon(
+                icon!.icon,
+                color: textColor,
+                size: iconSize,
+              ),
+            ),
+          if (text != null)
+            Text(
+              text!,
+              style: TextStyle(color: textColor),
+            ),
+          if (icon != null && isIconLeft)
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Icon(
+                icon!.icon,
+                color: textColor,
+                size: iconSize,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/molecules_gallery.dart
+++ lib/pages/molecules_gallery.dart
@@ -1,21 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:widgetr/design/molecules/custom_button.dart';
+import 'package:widgetr/design/theme/design_theme.dart';
 
 class MoleculesGallery extends StatelessWidget {
   const MoleculesGallery({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final exampleTheme = DesignTheme.light();
+
+    Widget buildButtons(DesignTheme theme) {
+      buildPrimaryButton({Icon? icon, required String text, double? iconSize}) {
+        return CustomButton(
+          text: text,
+          onPressed: () => print("$text pressed"),
+          backgroundColor: theme.colorPallet.primary,
+          textColor: theme.colorPallet.onPrimary,
+          icon: icon,
+          iconSize: iconSize ?? theme.iconSizes.largeSize,
+        );
+      }
+
+      buildSecondaryButton(
+          {Icon? icon, required String text, double? iconSize}) {
+        return CustomButton(
+          text: text,
+          onPressed: () => print("$text pressed"),
+          backgroundColor: Colors.white,
+          textColor: theme.colorPallet.onSecondary,
+          icon: icon,
+          iconSize: iconSize ?? theme.iconSizes.largeSize,
+          borderColor: theme.colorPallet.secondary,
+          borderWidth: theme.borders.mediumWidth,
+          borderRadius: theme.borders.mediumRadius,
+        );
+      }
+
+      buildErrorButton({Icon? icon, required String text, double? iconSize}) {
+        return CustomButton(
+          text: text,
+          onPressed: () => print("$text pressed"),
+          backgroundColor: theme.colorPallet.error,
+          textColor: theme.colorPallet.onError,
+          icon: icon,
+          iconSize: iconSize ?? theme.iconSizes.largeSize,
+        );
+      }
+
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          buildPrimaryButton(
+            text: "Primary Button with Icon",
+            icon: Icon(Icons.settings),
+            iconSize: theme.iconSizes.largeSize,
+          ),
+          const SizedBox(height: 10),
+          buildPrimaryButton(
+            text: "Primary Button without Icon",
+            iconSize: theme.iconSizes.mediumSize,
+          ),
+          const SizedBox(height: 10),
+          buildPrimaryButton(
+            text: "Primary Button with Smaller Icon",
+            icon: Icon(Icons.star),
+            iconSize: theme.iconSizes.smallSize,
+          ),
+          const SizedBox(height: 10),
+          buildSecondaryButton(
+            text: "Secondary Button with out Icon",
+          ),
+          const SizedBox(height: 10),
+          buildErrorButton(
+            text: "Error Button with out Icon",
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Molecules'),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Welcome to Molecules Gallery'),
-            SizedBox(height: 20),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40),
+              const Text('Buttons'),
+              const SizedBox(height: 20),
+              buildButtons(exampleTheme),
+              const SizedBox(height: 40),
+              const Text('Icon Sizes'),
+              const SizedBox(height: 20),
+              const SizedBox(height: 400),
+            ],
+          ),
         ),
       ),
     );
